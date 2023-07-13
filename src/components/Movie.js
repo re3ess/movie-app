29f3f1d5e9355114477761d2b8984ddb5bbf9394
DK <shamo1995@naver.com>
2023-07-13 16:51:21 +0900

    first

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function Movie({ id, coverImg, title, summary, genres, year }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <p>{year}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
